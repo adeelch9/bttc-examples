3fad1597fde22059d1c8352658c0dc98eec2dcf6
Adeel Chaudhary <adeel.chaudhary@tron.network>
2024-01-29 23:37:22 +0500

    fix projects for package lock json

--- projects/erc20/scripts/deploy.js
+++ projects/erc20/scripts/deploy.js
@@ -5,7 +5,9 @@ async function main() {
 
   const tokenName = "Simple ERC20";
   const tokenSymbol = "SERC20";
-  const totalSupply = 1000000 * math.pow(10, 18);
+
+  // 100000 * 10^18
+  const totalSupply = "100000000000000000000000";
   const simpleERC20ContractDeployed = await simpleERC20.deploy(tokenName, tokenSymbol, totalSupply);
   console.log("ERC20 deployed to address: ", simpleERC20ContractDeployed.target);
 }
